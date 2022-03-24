d1ca546b9784a4699758356d0a55e92e8403d678
suraj807 <surajdev805@gmail.com>
2022-03-24 16:19:44 +0530

    [suraj]  Line Comparison Problem

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Problem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line Comparison computation");
+        }
+    }
+}
